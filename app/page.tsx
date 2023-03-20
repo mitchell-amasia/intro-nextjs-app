45219ab21a150315a655c1f9233a4a9664955f38
Mitchell Amasia <mitchell.amasia@dotcontrol.com>
2023-03-20 16:03:23 -0400

    Started creating blog. Nog working yet. Getting Runtime Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-
+import Link from 'next/link'
+import styles from '../styles/home.module.css'
 
 export default async function Home() {
-  return <div>hello</div>
+  return (
+    <div className={styles.home}>
+      <div><h1>Hi, my name is Mitchell Amasia</h1></div>
+      <div>
+        <Link href="/blog">Checkout my blog!</Link>
+      </div>
+      <div>
+      <Link href="/contact">Contact me</Link>
+
+      </div>
+    </div>
+  )
 }
